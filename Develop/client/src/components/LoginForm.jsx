e3a0b2b7114bd5bd83d2725de29985fdf0e19784
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-04-02 23:17:33 -0300

    Switched to useMutatations

--- Develop/client/src/components/LoginForm.jsx
+++ Develop/client/src/components/LoginForm.jsx
@@ -6,6 +6,7 @@ import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   // Note: Corrected the useMutation hook with proper error handling
