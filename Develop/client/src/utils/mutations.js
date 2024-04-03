1c86b72a63ff9c585e621f5e798f25b757761192
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-04-02 22:07:16 -0300

    added mutations + queries to Front end

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client';
+
+// User login 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Add a User 
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token 
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+// Save a book 
+export const SAVE_BOOK = gql`
+mutation saveBook ($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+        _id
+        username
+        email
+        savedBooks{
+            bookId
+            authors
+            title
+            description
+            image
+            link
+        }
+    }
+}
+`;
+
+// Remove a book from savedBooks
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+    removeBooks(bookId: $bookId) {
+        _id 
+        savedBooks{
+            bookId
+            authors
+            title
+            description
+            image
+            link
+        }
+    }
+}
+`;
